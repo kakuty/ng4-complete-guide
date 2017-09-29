1e9ed82633cb453ef1f4d5d538c0ada4c5691a87
heyuno <haihoang09@yahoo.com>
2017-09-29 17:36:39 +0700

    Finish the layout

---
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from './../recipe.module';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A recipe', 'This is simple text',
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
